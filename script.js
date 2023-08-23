2d67d4bfcd4ccc6c648c1141607838ea657e43a4
goltsvlad <142609330+goltsvlad@users.noreply.github.com>
2023-08-23 12:06:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -69,8 +69,8 @@ function renderProducts() {
           product.count > 0
             ? `<div class="count-badge">${product.count}</div>`
             : ''
-          <button class="menu-button" onclick="toggleDescription()">≡</button>
         }
+        <button class="menu-button" onclick="toggleDescription()">≡</button>
       </div>
       <div class="product-name">${product.name}</div>
       <div class="product-price">${product.price} рублей</div>
