ceeb6d9c1aeaeebe8f8dbacd5133bc0d91d9b43a
goltsvlad <142609330+goltsvlad@users.noreply.github.com>
2023-08-29 00:45:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -147,7 +147,7 @@ function toggleDescriptionPopup(productId) {
       descriptionPopup.classList.add('animate-slide-up');
       tg.BackButton.show();
       tg.MainButton.hide();
-    }, 0); // Задержка в миллисекундах
+    }, 500); // Задержка в миллисекундах
   }
 }
 
