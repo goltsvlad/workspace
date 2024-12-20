95a37ee2cc3422f7532c081ece094a8779c327e8
goltsvlad <goltsvlad@gmail.com>
2024-12-20 20:16:15 +0000

    index.html (M) index.py (M)

--- index.py
+++ index.py
@@ -230,8 +230,9 @@
         color: #333; /* Цвет текста */
         line-height: 1.5; /* Межстрочное расстояние */
         margin-top: 10px;
+        padding: 10px;
         margin: 10px;
-        width: 100%;
+        width: calc(100% - 10px);
         border-radius: 10px;
     }
     
